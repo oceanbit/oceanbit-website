77abe14eb30c8b576730f2dc84d39a9ba6d83116
Corbin Crutchley <crutchcorn@gmail.com>
2020-07-30 16:35:51 -0700

    Got variadic fonts working

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
       resolve: "gatsby-plugin-web-font-loader",
       options: {
         google: {
-          families: [`Roboto Mono:400`, `Epilogue:400,500,600,700`],
+          families: [`Roboto Mono:400`],
         },
       },
     },
