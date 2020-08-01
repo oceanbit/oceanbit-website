219fa5d0dbbf1b91712be8a2ee9d5d12993a13be
Corbin Crutchley <crutchcorn@gmail.com>
2020-07-31 18:45:53 -0700

    Added read more button link to blog post list

--- src/templates/post-list/post-list.jsx
+++ src/templates/post-list/post-list.jsx
@@ -4,6 +4,9 @@ import SEO from "../../components/seo"
 import { Pagination } from "../../components/pagination"
 import { Layout } from "../../components/layout"
 import style from './post-list.module.scss';
+import { ButtonBase } from "@material-ui/core"
+import styles from "../../pages/contributions.module.scss"
+import ChevronIcon from "../../assets/chevron.svg"
 
 const BlogPostListTemplate = ({ pageContext }) => {
 	const { posts, pageIndex, pageCount } = pageContext
@@ -29,9 +32,10 @@ const BlogPostListTemplate = ({ pageContext }) => {
                 <h2 className={style.postTitle}>{post.frontmatter.title}</h2>
                 <p className={style.date}>July <br/><span className={style.day}>31</span></p>
                 <p className={style.excerpt}>{post.excerpt}</p>
+                <ButtonBase className={style.readMore}>
+                   Read more <ChevronIcon/>
+                </ButtonBase>
               </div>
-
-
             </Link>
           </li>
         ))}
