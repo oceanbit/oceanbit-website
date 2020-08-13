e8a0ade6a0062fd57cd29b38fd1b6f03a2603ff0
Corbin Crutchley <crutchcorn@gmail.com>
2020-08-12 22:59:37 -0700

    Migrate index page to styled components

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,22 @@
 import React from "react"
 import SEO from "../components/seo"
 import { Layout } from "../components/layout"
-import styles from "./index.module.scss"
 import { Screens } from "../components/screens"
 import ChevronIcon from "../assets/chevron.svg"
 import { useWave } from "../components/wave"
-import { Link } from "gatsby"
 import Ink from "react-ink"
+
+import {
+  ContributingBody,
+  ContributingHeader,
+  ContributionsButton,
+  HeadingBody,
+  HeadingBodyContainer,
+  HeadingContainer,
+  HeadingContents, HeadingTitle,
+  MainContents, MeetButton,
+  RelativeContainer
+} from "./index.styles"
 
 const IndexPage = () => {
   const { wave, waveHeight, marginTopBelow } = useWave("var(--base)", "bottom")
@@ -19,36 +29,36 @@ const IndexPage = () => {
           content: `website`
         }
       ]}/>
-      <div className={styles.relativeContainer}>
-        <div className={styles.headingContainer} style={{ paddingBottom: waveHeight }}>
-          <div className={styles.headingContents}>
-            <div className={styles.headingBodyContainer}>
-              <p className={styles.headingTitle}>Surf through your repos, wherever you go</p>
-              <p className={styles.headingBody}>Manage repositories, checkout branches, write and commit changes and
-                much more, right from your mobile device</p>
-                <a className={`${styles.meetButton} ${styles.mainButton}`} href={"https://gitshark.dev"}>Meet
-                  GitShark <ChevronIcon/>
-                  <Ink/>
-                </a>
-            </div>
+      <RelativeContainer>
+        <HeadingContainer style={{ paddingBottom: waveHeight }}>
+          <HeadingContents>
+            <HeadingBodyContainer>
+              <HeadingTitle>Surf through your repos, wherever you go</HeadingTitle>
+              <HeadingBody>Manage repositories, checkout branches, write and commit changes and
+                much more, right from your mobile device</HeadingBody>
+              <MeetButton href={"https://gitshark.dev"}>Meet
+                GitShark <ChevronIcon/>
+                <Ink/>
+              </MeetButton>
+            </HeadingBodyContainer>
             <Screens/>
-          </div>
-        </div>
+          </HeadingContents>
+        </HeadingContainer>
         {wave}
-      </div>
-      <div className={styles.mainContents} style={{ marginTop: marginTopBelow }}>
+      </RelativeContainer>
+      <MainContents style={{ marginTop: marginTopBelow }}>
         <div className="mainContents">
           <div>
-            <h3 className={styles.contributingHeader}>Contributing back</h3>
-            <p className={styles.contributingBody}>At OceanBit we believe in open code, which is why we’re always happy
-              to support open-source projects and share our own code as well</p>
-              <Link to={"/contributions"} className={`${styles.contributionsButton} ${styles.mainButton}`}>OceanBit’s
-                contributions <ChevronIcon/>
-                <Ink/>
-              </Link>
+            <ContributingHeader>Contributing back</ContributingHeader>
+            <ContributingBody>At OceanBit we believe in open code, which is why we’re always happy
+              to support open-source projects and share our own code as well</ContributingBody>
+            <ContributionsButton to={"/contributions"}>OceanBit’s
+              contributions <ChevronIcon/>
+              <Ink/>
+            </ContributionsButton>
           </div>
         </div>
-      </div>
+      </MainContents>
     </Layout>
   )
 }
---
+++ src/pages/index.styles.js
@@ -0,0 +1,114 @@
+import styled, { css } from "styled-components"
+import { forDesktop } from "../styles/utils"
+import { desktopPadding, maxContentWidth, mobilePadding } from "../styles/vars"
+import { body_01, callout_01, headline_02, headline_02_mobile } from "../styles/font-styles"
+import { Link } from "gatsby"
+
+export const RelativeContainer = styled.div`
+  position: relative;
+`
+
+export const HeadingContainer = styled.div`
+  background: var(--primary);
+  overflow: hidden;
+  position: relative;
+`
+
+export const HeadingContents = styled.div`
+  padding-bottom: 50px;
+  max-width: ${maxContentWidth};
+  margin: 0 auto;
+  padding-left: ${mobilePadding};
+  padding-right: ${mobilePadding};
+
+  ${forDesktop(`
+    padding-bottom: 240px;
+    padding-left: ${desktopPadding};
+    padding-right: ${desktopPadding};
+  `)}
+`
+
+export const HeadingBodyContainer = styled.div`
+  max-width: 456px;
+
+  margin-top: 24px;
+  position: relative;
+  z-index: 1;
+
+  ${forDesktop(`
+    margin-top: 96px;
+  `)}
+`
+
+export const HeadingTitle = styled.p`
+  ${headline_02_mobile}
+  color: var(--base);
+
+  ${forDesktop(`
+    ${headline_02}
+  `)}
+`
+
+export const HeadingBody = styled.p`
+  ${body_01}
+  color: var(--base);
+  margin-top: 8px;
+  margin-bottom: 24px;
+`
+
+const MainButton = css`
+  ${callout_01}
+  padding-left: 16px;
+  padding-top: 8px;
+  padding-bottom: 8px;
+  padding-right: 8px;
+  border: none;
+  display: inline-flex;
+  align-items: center;
+  text-decoration: none;
+  overflow: hidden;
+  border-radius: 8px !important;
+  overflow: hidden;
+  position: relative;
+
+  > svg {
+    margin-left: 4px;
+    height: 1.5rem;
+    width: 1.5rem;
+  }
+`
+
+export const MeetButton = styled.a`
+  ${MainButton}  
+  color: var(--primary);
+  background: var(--base);
+`
+
+export const ContributionsButton = styled(Link)`
+  ${MainButton}
+  color: var(--base);
+  background: var(--primary);
+`
+
+export const MainContents = styled.div`
+  position: relative;
+  z-index: 1;
+  margin-bottom: 10rem;
+`
+
+export const ContributingHeader = styled.h3`
+  ${headline_02_mobile}
+  color: var(--highEmphasis);
+
+  ${forDesktop(`
+    ${headline_02}
+  `)}
+`
+
+export const ContributingBody = styled.p`
+  ${body_01}
+  color: var(--mediumEmphasis);
+  margin-top: 8px;
+  margin-bottom: 24px;
+  max-width: 465px;
+`
