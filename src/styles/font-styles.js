3cabfa912d71b1fb229fd6724cdec5b0abbf19f7
Corbin Crutchley <crutchcorn@gmail.com>
2020-08-15 06:23:23 -0700

    Migrate font styles to shared package

--- src/styles/font-styles.js
+++ src/styles/font-styles.js
@@ -1,94 +1,71 @@
 import { css } from "styled-components"
-import { classToCSS } from "./base/utils"
+import { classToCSS } from "./transform-utils"
 
-import {
-  display_01 as display_01_base,
-  display_02 as display_02_base,
-  headline_01 as headline_01_base,
-  headline_01_mobile as headline_01_mobile_base,
-  headline_02 as headline_02_base,
-  headline_02_mobile as headline_02_mobile_base,
-  headline_03 as headline_03_base,
-  headline_03_mobile as headline_03_mobile_base,
-  headline_04 as headline_04_base,
-  headline_04_mobile as headline_04_mobile_base,
-  headline_05 as headline_05_base,
-  headline_05_mobile as headline_05_mobile_base,
-  headline_06 as headline_06_base,
-  callout_01 as callout_01_base,
-  callout_02 as callout_02_base,
-  body_01 as body_01_base,
-  body_02 as body_02_base,
-  caption_01 as caption_01_base,
-  caption_02 as caption_02_base,
-  overline_01 as overline_01_base,
-  overline_02 as overline_02_base,
-  code as code_base,
-} from "./base/font-styles"
+import {textStyles} from '@oceanbit/styles';
 
 export const display_01 = css`
-  ${classToCSS(display_01_base)}
+  ${classToCSS(textStyles.display_01)}
 `
 export const display_02 = css`
-  ${classToCSS(display_02_base)}
+  ${classToCSS(textStyles.display_02)}
 `
 export const headline_01 = css`
-  ${classToCSS(headline_01_base)}
+  ${classToCSS(textStyles.headline_01)}
 `
 export const headline_01_mobile = css`
-  ${classToCSS(headline_01_mobile_base)}
+  ${classToCSS(textStyles.headline_01_mobile)}
 `
 export const headline_02 = css`
-  ${classToCSS(headline_02_base)}
+  ${classToCSS(textStyles.headline_02)}
 `
 export const headline_02_mobile = css`
-  ${classToCSS(headline_02_mobile_base)}
+  ${classToCSS(textStyles.headline_02_mobile)}
 `
 export const headline_03 = css`
-  ${classToCSS(headline_03_base)}
+  ${classToCSS(textStyles.headline_03)}
 `
 export const headline_03_mobile = css`
-  ${classToCSS(headline_03_mobile_base)}
+  ${classToCSS(textStyles.headline_03_mobile)}
 `
 export const headline_04 = css`
-  ${classToCSS(headline_04_base)}
+  ${classToCSS(textStyles.headline_04)}
 `
 export const headline_04_mobile = css`
-  ${classToCSS(headline_04_mobile_base)}
+  ${classToCSS(textStyles.headline_04_mobile)}
 `
 export const headline_05 = css`
-  ${classToCSS(headline_05_base)}
+  ${classToCSS(textStyles.headline_05)}
 `
 export const headline_05_mobile = css`
-  ${classToCSS(headline_05_mobile_base)}
+  ${classToCSS(textStyles.headline_05_mobile)}
 `
 export const headline_06 = css`
-  ${classToCSS(headline_06_base)}
+  ${classToCSS(textStyles.headline_06)}
 `
 export const callout_01 = css`
-  ${classToCSS(callout_01_base)}
+  ${classToCSS(textStyles.callout_01)}
 `
 export const callout_02 = css`
-  ${classToCSS(callout_02_base)}
+  ${classToCSS(textStyles.callout_02)}
 `
 export const body_01 = css`
-  ${classToCSS(body_01_base)}
+  ${classToCSS(textStyles.body_01)}
 `
 export const body_02 = css`
-  ${classToCSS(body_02_base)}
+  ${classToCSS(textStyles.body_02)}
 `
 export const caption_01 = css`
-  ${classToCSS(caption_01_base)}
+  ${classToCSS(textStyles.caption_01)}
 `
 export const caption_02 = css`
-  ${classToCSS(caption_02_base)}
+  ${classToCSS(textStyles.caption_02)}
 `
 export const overline_01 = css`
-  ${classToCSS(overline_01_base)}
+  ${classToCSS(textStyles.overline_01)}
 `
 export const overline_02 = css`
-  ${classToCSS(overline_02_base)}
+  ${classToCSS(textStyles.overline_02)}
 `
 export const code = css`
-  ${classToCSS(code_base)}
+  ${classToCSS(textStyles.code)}
 `
