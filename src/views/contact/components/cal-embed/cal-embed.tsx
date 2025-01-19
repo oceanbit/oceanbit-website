e58e78c490b43fd5bb38d527b49103c851e70c47
Corbin Crutchley <git@crutchcorn.dev>
2025-01-19 02:58:16 -0800

    chore: move to Cal.com team

--- src/views/contact/components/cal-embed/cal-embed.tsx
+++ src/views/contact/components/cal-embed/cal-embed.tsx
@@ -1,7 +1,5 @@
 import Cal from "@calcom/embed-react";
 
 export const CalEmbed = () => {
-  return (
-    <Cal calLink="crutchcorn/long-meeting" config={{ theme: "light" }}></Cal>
-  );
+  return <Cal calLink="team/oceanbit/intro" config={{ theme: "light" }}></Cal>;
 };
