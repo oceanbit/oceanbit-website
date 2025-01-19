e4eaf2557c8d9d1f58631a4cce2cf7468f7461f5
Corbin Crutchley <git@crutchcorn.dev>
2025-01-19 01:54:24 -0800

    feat: add initial contact page code with Cal embed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 export default defineConfig({
+  integrations: [react()],
   vite: {
     css: {
       preprocessorOptions: {
---
+++ src/views/contact/components/cal-embed/cal-embed.tsx
@@ -0,0 +1,7 @@
+import Cal from "@calcom/embed-react";
+
+export const CalEmbed = () => {
+  return (
+    <Cal calLink="crutchcorn/long-meeting" config={{ theme: "light" }}></Cal>
+  );
+};
