846bf8f0fd40301d0b464663a08e4a9e132c43eb
Corbin Crutchley <git@crutchcorn.dev>
2025-02-01 01:06:45 -0800

    feat!: clean slate gaming

--- src/constants/theme.js
+++
@@ -1,8 +0,0 @@
-import {lightTheme, darkTheme} from '@oceanbit/styles';
-
-export const theme = {
-  light: lightTheme.colors,
-  dark: darkTheme.colors
-}
-
-export const INITIAL_COLOR_MODE_CSS_PROP = "--initial-color-mode"
--- src/styles/vars.js
+++
@@ -1,5 +0,0 @@
-export const mobilePadding = "18px"
-export const desktopPadding = "48px"
-export const mobileTop = "24px"
-export const desktopTop = "96px"
-export const maxContentWidth = "958px"
